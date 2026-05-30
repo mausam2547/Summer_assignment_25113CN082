d2efe5b2f9f9be7b34eebba6994488710be3256f
mausam2547 <mausamgangwar2906@gmail.com>
2026-05-30 15:09:13 +0530

    add Day-4

---
+++ Day-4/Question14.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int n, i, a = 0, b = 1, c;
+
+
+  printf("Enter the value of n: ");
+  scanf("%d", &n);
+
+  if (n == 1)
+  printf("The %dst Fibonacci term is %d", n, a);
+  else if (n == 2)
+  printf("The %dnd Fibonacci term is %d", n, b);
+  else
+  {
+    for (i = 3; i <= n; i++)
+      {
+        c = a + b;
+        a = b;
+        b = c;
+      }
+      printf("The %dth Fibonacci term is %d", n, b);
+  }
+
+  return 0;
+}
